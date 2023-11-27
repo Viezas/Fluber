bc6ee41f980bcf1bfb47ec8eb823d30cb2692400
skweel <dissingardjino@icloud.com>
2023-11-27 12:39:45 +0100

    mise en place du projet

--- lib/view/my_google_maps.dart
+++ lib/view/my_google_maps.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MyGoogleMaps extends StatefulWidget {
   final Position myPosition;
@@ -15,10 +16,17 @@ class MyGoogleMaps extends StatefulWidget {
 class _MyGoogleMapsState extends State<MyGoogleMaps> {
   late CameraPosition cameraPosition;
   Completer<GoogleMapController> controller = Completer();
+  Set<Marker>allMarkers = Set();
+
 
   @override
   void initState() {
     // TODO: implement initState
+    allMarkers.add(
+        Marker(
+            markerId: MarkerId("dfhskjfhdskjfh"),
+            position: LatLng(widget.myPosition.latitude, widget.myPosition.longitude),)
+    );
     cameraPosition = CameraPosition(target: LatLng(widget.myPosition.latitude, widget.myPosition.longitude),zoom: 14);
     super.initState();
   }
@@ -26,12 +34,17 @@ class _MyGoogleMapsState extends State<MyGoogleMaps> {
   Widget build(BuildContext context) {
     return GoogleMap(
         initialCameraPosition: cameraPosition,
+
+      markers: allMarkers,
       myLocationEnabled: true,
       myLocationButtonEnabled: true,
       onMapCreated: (control) async {
+
           String newStyle = await DefaultAssetBundle.of(context).loadString("lib/newStyle.json");
           control.setMapStyle(newStyle);
           controller.complete(control);
+
+
       },
     );
   }
